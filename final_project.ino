7194b9280021edf4b4fb9c8a814e539932e45ef0
Yugo Iwamoto <124512658+yu5july8@users.noreply.github.com>
2023-11-06 11:39:58 -0500

    Add files via upload

---
+++ final_project.ino
@@ -0,0 +1,24 @@
+#include <Grove_LED_Bar.h>
+
+#define LED_NUM 24
+Grove_LED_Bar LEDBar(0,1,0,LED_CIRCULAR_24); //clock pin, data pin and orientation
+
+int sound_sensor = A2; //assign to pin A2
+
+
+void setup() {
+  Serial.begin(115200); //begin Serial Communication
+
+}
+
+void loop() {
+  int soundValue = 0; //create variable to store many different readings
+  for (int i = 0; i < 32; i++) //create a for loop to read 
+  { soundValue += analogRead(sound_sensor);  } //read the sound sensor
+ 
+  soundValue >>= 5; //bitshift operation 
+  Serial.println(soundValue); //print the value of sound sensor
+  int level=map(soundValue, 0,90, 0, 24);
+  delay(5000); //a shorter delay between readings
+
+}
